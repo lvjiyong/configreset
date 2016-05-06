460a1570d0ae95de2b9f9bee526e22185f0349a7
吕继勇 <lvjiyong@19lou.com>
2016-05-06 14:59:35 +0800

    init

---
+++ configreset/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Created by lvjiyong on 16/5/6
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from os.path import dirname, join
+
+from setuptools import setup, find_packages
+
+project_dir = dirname(__file__)
+
+with open(join(project_dir, 'VERSION'), 'rb') as f:
+    version = f.read().decode('ascii').strip()
+
+setup(
+    name="ConfigReset",
+    version=version,
+    description="ConfigReset",
+    author="lvjiyong",
+    url="https://github.com/lvjiyong/configreset",
+    license="GPL",
+    include_package_data=True,
+    packages=find_packages(exclude=()),
+    long_description=open(join(project_dir, 'README')).read(),
+    maintainer='lvjiyong',
+    platforms=["any"],
+    maintainer_email='lvjiyong@gmail.com',
+    install_requires=[],
+)
